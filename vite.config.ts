e1bfb051a187f530852bd727d74573e959d322bf
[prasannagiri] <[prasannagiri12p@gmail.com]>
2025-12-15 02:04:03 +0545

    fixxx

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
       : []),
   ],
 
-    base: process.env.VITE_BASE_PATH || "/",
+    base: process.env.VITE_BASE_PATH || "/Rajeshwori-construction-",
 
 
   resolve: {
